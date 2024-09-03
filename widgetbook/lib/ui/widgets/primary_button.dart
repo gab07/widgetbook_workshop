509d43ed72a592670539ecec05410027ee73f066
gabriel <gabrielaguirre921@gmail.com>
2024-09-03 16:44:10 +0200

    feat: added primary button to widgetbook

--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -7,3 +7,14 @@ import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
 /// Add Widgetbook use-case here
+@UseCase(
+  name: 'Default',
+  type: PrimaryButton,
+  // designLink:
+  // 'https://www.figma.com/design/HsANkdhbsCNTkXBzNJRNLD/Groceries-Demo?node-id=7271-52117&t=N6qwmLP7MP59ClWB-4',
+)
+Widget buildPrimaryButtonUseCase(BuildContext context) {
+  return PrimaryButton(
+    content: context.knobs.string(label: 'content'),
+  );
+}
